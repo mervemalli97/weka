c47a6c8d2b891d7147266d05f818a5b6508ca20d
Merve Mallı <mervemalli97@gmail.com>
2020-01-16 22:13:50 +0300

    can now remove chosen attr

--- src/take/one/LoadnSave.java
+++ src/take/one/LoadnSave.java
@@ -2,10 +2,14 @@ package take.one;
 
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
 import java.io.File;
 
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.unsupervised.attribute.Remove;
 public class LoadnSave{
 	public static void main(String args[]) throws Exception{
 		DataSource source = new DataSource("data/iris.arff");
@@ -13,8 +17,15 @@ public class LoadnSave{
 		
 		System.out.println(dataset.toSummaryString());
 		
+		String[] opts = new String[]{ "-R", "1"};
+		// this takes two arguments, -R (range) is the option and 1 is the value to process
+		Remove remove = new Remove(); // new instance of filter
+		remove.setOptions(opts);
+		remove.setInputFormat(dataset);
+		Instances newData = Filter.useFilter(dataset, remove);
+
 		ArffSaver saver = new ArffSaver();
-		saver.setInstances(dataset);
+		saver.setInstances(newData);
 		saver.setFile(new File("data/new.arff"));
 		saver.writeBatch();
 	}
